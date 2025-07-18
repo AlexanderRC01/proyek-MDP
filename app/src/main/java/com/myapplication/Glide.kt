54c450dd06e420d31407d0431c9582ada9496171
akilcess <82573394+akilcess@users.noreply.github.com>
2025-07-18 14:12:46 +0700

    Delete app/src directory

--- app/src/main/java/com/myapplication/Glide.kt
+++
@@ -1,5 +0,0 @@
-package com.myapplication
-
-class Glide {
-
-}
--- app/src/main/java/com/myapplication/User.kt
+++
@@ -1,8 +0,0 @@
-package com.myapplication
-
-data class User(
-    val uid: String = "",
-    val name: String = "",
-    val emailAddress: String = "",
-    val role: String = ""
-)
